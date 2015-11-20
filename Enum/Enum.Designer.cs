053aa2f3435b335db61fb079791279bab328103d
RyanStLouis <ryan.stlouis@ocsbstudent.ca>
2015-11-20 12:59:48 -0500

    enum

---
+++ Enum/Enum.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Enum
+{
+    partial class frmEnum
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstWeek = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstWeek
+            // 
+            this.lstWeek.FormattingEnabled = true;
+            this.lstWeek.Items.AddRange(new object[] {
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday"});
+            this.lstWeek.Location = new System.Drawing.Point(54, 46);
+            this.lstWeek.Name = "lstWeek";
+            this.lstWeek.Size = new System.Drawing.Size(135, 108);
+            this.lstWeek.TabIndex = 1;
+            this.lstWeek.SelectedIndexChanged += new System.EventHandler(this.lstWeek_SelectedIndexChanged);
+            // 
+            // frmEnum
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 209);
+            this.Controls.Add(this.lstWeek);
+            this.Name = "frmEnum";
+            this.Text = "Enum";
+            this.Load += new System.EventHandler(this.frmEnum_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstWeek;
+    }
+}
+
---
+++ Enum/Enum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enum
+{
+    public partial class frmEnum : Form
+    {
+        // global variables
+        enum DAYS
+        {
+            sunday,
+            monday,
+            tuesday,
+            wednesday,
+            thursday,
+            friday,
+            saturday
+        }
+         
+        public frmEnum()
+        {
+            InitializeComponent();
+        }
+
+        private void frmEnum_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void lstWeek_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // say what item you selected
+            string selectValue;
+
+            selectValue = Convert.ToString(lstWeek.SelectedItem);
+
+            MessageBox.Show("Your favourite day of the week is " + selectValue);
+        }
+    }
+}
